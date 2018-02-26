cbed77fa2a482d6dda701b1171d9c9dd03045911
tcalhounjr <thomas@nafasi.co>
2018-02-25 23:13:00 -0500

    printed html to screen and js file started

---
+++ assets/javascript/game.js
@@ -0,0 +1,11 @@
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+}
+
+function matchLetter {
+   doSomething;
+}
+
+var psychicGame = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
